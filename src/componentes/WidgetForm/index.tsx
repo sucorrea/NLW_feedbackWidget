660abe24d5e5ae4666b0c07b9292c67cd7ab160d
Sueli Correa de Oliveira <oliver.sueli@gmail.com>
2022-05-11 10:50:45 -0300

    ATUALIZANDO

--- src/componentes/WidgetForm/index.tsx
+++ src/componentes/WidgetForm/index.tsx
@@ -45,10 +45,18 @@ const WidgetForm = () => {
         <span className="text-xl leading-6">Deixe seu Feedback</span>
         <CloseButton />
       </header>
-      <div className="flex py-8 gab-2 w-full">
+      <div className="flex py-8 gap-2 w-full">
         {Object.entries(feedbackTypes).map(([key, value]) => {
-          console.log(key, value);
-          return null;
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center"
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
         })}
       </div>
       <footer>
